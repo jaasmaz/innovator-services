272e7527cf163910ad430055141eb654a29efb88
jaasmaz <jaasmaz@gmail.com>
2024-02-01 13:05:43 -0800

    Connect Customer service to DB

--- customer/src/main/java/com/innovator/customer/CustomerService.java
+++ customer/src/main/java/com/innovator/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.innovator.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -11,6 +11,6 @@ public record CustomerService() {
                 .email(request.email()).build();
         // todo: check if email is valid
         // todo: check if email is not taken
-        // todo: store customer in db
+        customerRepository.save(customer);
     }
 }
